2c2669975273e079b52b8f1c9a5083317cabc160
Tristan Pemble <tristan.p@hostdime.com>
2014-04-28 11:17:55 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# hdcore-php
+
+A simple wrapper around [HostDime.com](http://www.hostdime.com/)'s client API.
+
+## Installing via Composer
+
+The recommended method of installation is via [Composer](http://getcomposer.org)
+
+    # Install Composer
+    curl -sS https://getcomposer.org/installer | php
+
+    # Add the library as a dependency
+    php composer.phar require hostdime/hdcore:~0.1
+
+## Usage
+
+~~~php
+$public_key = 'foo';
+$private_key = 'bar';
+$client = new \HostDime\Api\Client($public_key, $private_key);
+
+$servers = $client->call('server.list');
+
+for ($servers as $server) {
+    echo $server['name'];
+}
+~~~
+
+## Reference
+
+For a comprehensive overview of the HostDime API, please see our [API reference](https://api.hostdime.com/docs/).
+
+## License - MIT
+
+Copyright © 2013 HostDime.com, Inc.
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ src/HostDime/Api/Client.php
@@ -0,0 +1,82 @@
+<?php
+namespace HostDime\Api;
+
+class Client
+{
+    private $_endpoint = 'https://api.hostdime.com/v1';
+
+    public function __construct($publicKey, $privateKey, $options=NULL)
+    {
+        $this->_publicKey = $publicKey;
+        $this->_privateKey = $privateKey;
+
+        if ($options !== NULL) 
+        {
+            if (isset($options['endpoint']))
+                $this->_endpoint = $options['endpoint'];
+        }
+    }
+
+    public function call($action, $parameters=NULL)
+    {
+        if ($parameters === NULL)
+            $parameters = array();
+
+        $timestamp = self::generateTimestamp();
+        $unique = self::generateUuid();
+        $hash = self::generateHash($timestamp, $unique, $this->_privateKey, $action, $parameters);
+        $route = self::generateRoute($action);
+
+        $guzzle = new \Guzzle\Http\Client($this->_endpoint);
+
+        $request = $guzzle->post($route, null, array_merge($parameters, array(
+            'api_key' => $this->_publicKey,
+            'api_timestamp' => $timestamp,
+            'api_unique' => $unique,
+            'api_hash' => $hash
+        )));
+
+        $result = $request->send()->json();
+
+        if ($result['error'] !== NULL)
+            throw new Exception($result['error']);
+        else
+            return $result['response'];
+    }
+
+    private static function generateTimestamp()
+    {
+        return time();
+    }
+
+    private static function generateRoute($action)
+    {
+        return sprintf('%s.json', implode('/', explode('.', $action)));
+    }
+
+    private static function generateUuid()
+    {
+        return sprintf( '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+            mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
+            mt_rand( 0, 0xffff ),
+            mt_rand( 0, 0x0fff ) | 0x4000,
+            mt_rand( 0, 0x3fff ) | 0x8000,
+            mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
+        );
+    }
+
+    private static function generateHash($timestamp, $unique, $key, $action, $parameters)
+    {
+      $elements = array(
+        $timestamp,
+        $unique,
+        $key,
+        $action,
+        json_encode(json_encode($parameters, JSON_FORCE_OBJECT))
+      );
+      $string = implode(':',$elements);
+      $hash = hash("sha256", $string);
+      
+      return $hash;
+    }
+}
